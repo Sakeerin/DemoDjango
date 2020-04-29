d66b1ebdc5e78a881355cb796734d4e2502d1733
sakeerin <theblue0800@gmail.com>
2020-04-29 17:20:53 +0800

    Create new project and setup project

---
+++ HelloDjango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sakeerinkhami/Documents/python/DemoDjango/HelloDjango/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
